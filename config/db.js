38224940c623a47d5e612f84c73d4bb7f097d865
ShaniceeR <shanice.radhalal@gmail.com>
2024-05-26 23:09:26 +0930

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql');
-
-const db = mysql.createConnection({
-  host: 'localhost',
+const connection = mysql.createConnection({
+  host: ' coursesecurity ', // Use 'YOUR_INSTANCE_CONNECTION_NAME' for Cloud SQL
   user: 'root',
-  password: '',
+  password: 'Coursesecurity',
   database: 'course'
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('The Database is connected');
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database.');
 });
 
-module.exports = db;
+module.exports = connection;
